45f624baaa456389a1186745ea12b6a9c6db5624
Mads Holten Rasmussen <mads.h.rasmussen@gmail.com>
2018-01-04 10:29:54 +0100

    first commit

---
+++ PyS2D3.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+
+__author__      = "Mads Holten Rasmussen"
+__copyright__   = "Copyright 2017, DTU|Niras"
+__license__     = "GPL"
+__version__     = "0.0.1"
+__email__       = "mhoras@byg.dtu.dk"
+__status__      = "Development"
+
+import rdflib
+import argparse
+import json
+
+class PySPARQL2D3(object):
+
+    def __init__(self):
+        self.graph = rdflib.Graph()  # holds graph
+        self.prefixesPath = './data/prefixes.json'
+
+    def establishGraph(self,data,**kwargs):
+        # From string or file(s)?
+        string = kwargs.pop('string', False)
+
+        if not string:
+            # if data is a string, append it to a list
+            if type(data) is str:
+                data = [data]
+            # insert all data items to the graph and return the graph
+            for item in data:
+                self.graph.parse(item, format="turtle")
+        else:
+            self.graph.parse(data=data, format="turtle")
+
+        return self.graph
+
+    def generateD3Triples(self, query, **kwargs):
+        # Get optional arguments
+        prefixesPath = kwargs.pop('prefixesPath', self.prefixesPath)
+        filePath = kwargs.pop('filePath', None)
+
+        if '{' in query:
+            queryString = query
+        else:
+            # If query is in a file, read it into a string
+            with open(query, 'r') as f:
+                queryString = f.read()
+
+        # Perform query and get result
+        result = self.graph.query(queryString)
+
+        # If no result, return error
+        if len(result) == 0:
+            return "ERR: The query returned no result"
+        
+        # Parse result to D3 format
+        triples = self._parse2D3(result,prefixesPath)
+
+        # Write to file
+        with open(filePath, 'w') as outfile:
+            json.dump(triples, outfile)
+        outfile.close
+        
+        return triples
+    
+    def _parse2D3(self,triples,prefixesPath=None):
+        triplesJSON = list()
+
+        for s,p,o in triples:
+
+            # If prefixes are defined, append these
+            if prefixesPath:
+                prefixes = json.load(open(prefixesPath))
+
+                for item in prefixes:
+                    pfx = str(item["prefix"]+":")
+                    if item["uri"] in s:
+                        s = s.replace(item["uri"], pfx)
+                    if item["uri"] in p:
+                        p = p.replace(item["uri"], pfx)
+                    if item["uri"] in o:
+                        o = o.replace(item["uri"], pfx)
+
+            # Create JSON output
+            triplesJSON.append({"subject": s, "predicate": p, "object": o})
+
+        return triplesJSON
+
+def main():
+    # Get arguments from stdin
+    parser = argparse.ArgumentParser(description='Generate D3 JSON file')
+    parser.add_argument('-t', '--triples', nargs='+', required=True, help='Path to one or more triple files in ttl-format')
+    parser.add_argument('-q', '--query', required=True, help='SPARQL query string or path to a file containing the SPARQL query')
+    parser.add_argument('-p', '--prefixes', required=False, help='Path to JSON-file with prefixes. Defaults to ./data/prefixes.json')
+    parser.add_argument('-o', '--output', required=False, help='Path to output file. Defaults to ./data.json')
+
+    # Get arguments and set defaults
+    args = parser.parse_args()
+    paths = args.triples
+    query = args.query
+    filePath = args.output if args.output else './data.json'
+    prefixesPath = args.prefixes if args.prefixes else './data/prefixes.json'
+
+    # Initialize class
+    pd3 = PySPARQL2D3()
+
+    # Load data to in memory graph
+    g = pd3.establishGraph(paths)
+
+    # Do query and write to D3-json
+    d3JSON = pd3.generateD3Triples(query,filePath=filePath,prefixesPath=prefixesPath)
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,34 @@
+## USE
+```sh
+$ python PyS2D3.py -t fileA.ttl fileB.ttl -q query.rq
+```
+
+Arguments
+
+Flag            | Required | Description
+--------------- | -------- | -----------
+-t --triple     | X        | Path to one or more triple files in ttl-format
+-q --query      | X        | SPARQL query string or path to a file containing the SPARQL query
+-p --prefixes   |          | Path to JSON-file with prefixes. Defaults to ./data/prefixes.json
+-o --output     |          | Path to output file. Defaults to ./data.json
+
+## TEST
+```sh
+$ python PyS2D3.py -t ./test/fileA.ttl ./test/fileB.ttl -q ./test/query.rq -o ./test/data.json
+```
+
+## D3 JSON file use
+Based on the work by Rathachai, I have made some minor visual improvements and changed it so that it renders content from a JSON-file.
+Simply run a simple http server from the root folder. It visualises the file 'data.json' located within the folder as default.
+
+Run server with python 2.7
+```sh
+$ python -m SimpleHTTPServer 8000
+```
+
+Run server with python 3.6
+```sh
+$ python -m http.server 8000
+```
+
+Then visit localhost:8000 in the browser to see the result
